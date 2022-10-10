ce7df98daf87906fba9ca2a87c8a990e75d3e20d
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-10-10 10:33:00 -0400

    Initial Swagger UI Commit

---
+++ dist/swagger-initializer.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "swagger.yaml"
+  });
+
+  //</editor-fold>
+};
